ddb533fb4c6e8b23d0bd2b9a24275e32eea73b64
anthonyackermans <64217448+anthonyackermans@users.noreply.github.com>
2020-05-04 21:06:21 +0200

    Refactor and drape objects

---
+++ Runtime/Editor/DrapeOnObjects.cs
@@ -0,0 +1,86 @@
+﻿// Anthony Ackermans
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Move objects in a world space axis until it collides with an object. 
+/// Ideal for draping objects on a surface, like trees on a terrain.
+/// </summary>
+public class DrapeOnObjects : EditorWindow
+{
+    // FIELDS
+    public enum Axis { X, Y, Z };
+    private Axis _projectionAxis = Axis.Y;
+    private bool _inverseAxis;
+    private List<TransformElement> _transformElementsToMove = new List<TransformElement>();
+    private List<TransformElement> _transformElementsToProjectOn = new List<TransformElement>();
+    public ObjectGetter ObjectGetterToMove = new ObjectGetter();
+    public ObjectGetter ObjectGetterToProjectOn = new ObjectGetter();
+    private Vector3 _raycastDirection;
+    private string _logBox;
+    // Add menu item 
+    [MenuItem("RMDY AR VR/Drape on objects")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(DrapeOnObjects));
+    }
+
+    private void AlignObjectToNormal(Axis projectionAxis)
+    {
+        int objectsHit = 0;
+        int objectsMissed = 0;
+
+        switch (projectionAxis)
+        {
+            case Axis.X:
+                _raycastDirection = _inverseAxis == true ? Vector3.left : Vector3.right;
+                break;
+            case Axis.Y:
+                _raycastDirection = _inverseAxis == true ? Vector3.down : Vector3.up;
+                break;
+            case Axis.Z:
+                _raycastDirection = _inverseAxis == true ? Vector3.back : Vector3.forward;
+                break;
+            default:
+                break;
+        }
+
+        foreach (var transfomElementtoMove in _transformElementsToMove)
+        {
+            RaycastHit hit;
+
+
+            if (Physics.Raycast(transfomElementtoMove.TheGameObject.transform.position, _raycastDirection, out hit))
+                foreach (var transformElementtoProjectTo in _transformElementsToProjectOn)
+                {
+                    if (transformElementtoProjectTo.TheGameObject.transform == hit.transform)
+                    {
+                        transfomElementtoMove.TheGameObject.transform.position = hit.point;
+                        objectsHit++;
+                    }                  
+                }
+            else objectsMissed++;
+        }
+        _logBox = $"{objectsHit} objects draped \n{objectsMissed} objects missed";
+    }
+
+    private void OnGUI()
+    {
+        _transformElementsToMove = ObjectGetterToMove.ObjectSelectionShowUi("Search for transforms");
+        EditorGUILayout.Space(10);
+        _transformElementsToProjectOn = ObjectGetterToProjectOn.ObjectSelectionShowUi("Project on transforms");
+        EditorGUILayout.Space(10);
+        _projectionAxis = (Axis)EditorGUILayout.EnumPopup("Projection axis", _projectionAxis);
+        _inverseAxis = EditorGUILayout.Toggle("Inverse axis", _inverseAxis);
+
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.LabelField(_logBox, EditorStyles.helpBox, GUILayout.Height(30));
+        if (GUILayout.Button("Apply"))
+        {
+            ObjectGetter.RecordeUndoForSelectedObjects(_transformElementsToMove, "Project objects");
+            AlignObjectToNormal(_projectionAxis);
+        }
+    }
+}
---
+++ Runtime/Editor/TransformElement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TransformElement
+{
+
+    public GameObject TheGameObject;
+    public Vector3 originalPosition;
+    public Quaternion originalRotation;
+    public Vector3 originalScale;
+
+    public TransformElement(GameObject go, Transform tr)
+    {
+        TheGameObject = go;
+        originalPosition = tr.localPosition;
+        originalRotation = tr.localRotation;
+        originalScale = tr.localScale;
+    }
+
+
+}
